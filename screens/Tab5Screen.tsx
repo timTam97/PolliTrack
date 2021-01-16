4f071ce1ab5bddcc865166980b5995d06c49b1b9
Marcus Lim <marcus4lim@gmail.com>
2021-01-16 19:34:44 +1100

    Auto stash before merge of "master" and "origin/master"

--- screens/Tab5Screen.tsx
+++ screens/Tab5Screen.tsx
@@ -1,57 +1,92 @@
 import React from 'react';
-import {SafeAreaView, View, Text, Image, StyleSheet, ScrollView, TouchableHighlight} from 'react-native';
+import { useState } from 'react';
+import {SafeAreaView, View, Text, Image, StyleSheet, ScrollView, TouchableHighlight, TouchableOpacity} from 'react-native';
+import { Card } from 'react-native-paper';
+import { Agenda } from 'react-native-calendars';
 
-export default function Tab5Screen({navigation}) {
-  return (
-    <SafeAreaView style={styles.container}>
-      <View style={styles.greetingContainer}>
-        <Image
-          source={require('../assets/images/check.png')}
-          resizeMode="contain"
-          style={styles.placement}
-        ></Image>
-        <Text style={styles.title}>TY</Text>
-        <Text style={styles.subtitle}>Photos have been sent</Text>
+const timeToString = (time) => {
+  const date = new Date(time);
+  return date.toISOString().split('T')[0];
+};
 
+export default function Tab5Screen() {
+  const [items, setItems] = useState({});
+  const loadItems = (day) => {
+    setTimeout(() => {
+      for (let i = -15; i < 85; i++) {
+        const time = day.timestamp + i * 24 * 60 * 60 * 1000;
+        const strTime = timeToString(time);
+        if (!items[strTime]) {
+          items[strTime] = [];
+          const numItems = Math.floor(Math.random() * 3 + 1);
+          for (let j = 0; j < numItems; j++) {
+            items[strTime].push({
+              name: 'Medication for ' + strTime,
+              height: Math.max(50, Math.floor(Math.random() * 150)),
+            });
+          }
+        }
+      }
+      const newItems = {};
+      Object.keys(items).forEach((key) => {
+        newItems[key] = items[key];
+      });
+      setItems(newItems);
+    }, 1000);
+  };
+
+  const renderItem = (item) => {
+    return (
+      <TouchableOpacity>
+        <Card>
+          <Card.Content>
+            <View style={{ flexDirection: 'row', justifyContent: 'space-between', alignItems: 'center' }}>
+              <Text>{item.name}</Text>
+            </View>
+          </Card.Content>
+        </Card>
+      </TouchableOpacity>
+    );
+  };
+
+return (
+    <SafeAreaView style={{ flex: 1 }}>
+      <Text style={styles.title}>sales</Text>
+      <View style={styles.agenda}>
+        <Agenda
+
+          items={items}
+          loadItemsForMonth={loadItems}
+          selected={'2017-05-16'}
+          renderItem={renderItem}
+        />
       </View>
-      
+      <Text style={styles.month}>January</Text>
     </SafeAreaView>
+
   );
 }
 
-const styles = StyleSheet.create({
-  bottomContainer: {
-    position: 'absolute',
-    bottom: -180,
-    justifyContent: 'center',
-    width: 500,
-    height: 357,
-    flex: 2,
+export const styles = StyleSheet.create({
+  title: {
+    fontFamily: 'Lato_700Bold',
+    color: 'rgba(23,34,133,1)',
+    fontSize: 35,
+    marginTop: 25,
+    marginLeft: 34,
   },
   container: {
     flex: 1,
-    backgroundColor: '#ffffff',
   },
-  button: {
-    alignItems: 'center',
-    marginTop: 405,
+  month: {
+    fontFamily: 'Lato_700Bold',
+    color: 'rgba(23,34,133,1)',
+    fontSize: 17,
+    top: -530,
+    marginLeft: 34,
   },
-  placement: {
-    marginHorizontal: 110,
-    marginVertical:20
+  agenda: {
+    flex: 1,
+    top: 120,
   },
-  title: {
-    fontSize: 150,
-    // fontWeight: 'bold',
-    color: '#DE8C1E',
-    fontFamily: 'Roboto',
-    textAlign: 'center'
-  },
-  subtitle: {
-    fontSize: 50,
-    // fontWeight: 'bold',
-    color: '#DE8C1E',
-    fontFamily: 'Roboto',
-    textAlign: 'center'
-  }
 });
