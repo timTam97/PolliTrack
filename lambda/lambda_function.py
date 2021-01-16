1dc4559381454d3bbd2ae31c48ebfba709791ce7
Timothy Samraj <tsam0005@student.monash.edu>
2021-01-16 18:06:25 +1100

    Add working code for barcode grab

--- lambda/lambda_function.py
+++ lambda/lambda_function.py
@@ -2,53 +2,47 @@ import base64
 import datetime
 import hashlib
 import hmac
+import io
 import json
 import os
 import sys
+import time
 
 import boto3
 import requests
 from aws_requests_auth.aws_auth import AWSRequestsAuth
 
-
-def post_sagemaker(binary: str):
-    return {"statusCode": 200, "body": json.dumps("Hello from Lambda!")}
-    method = "POST"
-    service = "sagemaker"
-    region = "us-east-1"
-    endpoint = "https://runtime.sagemaker.us-east-1.amazonaws.com/endpoints/acchackendpoint/invocations"
-    request_parameters = "Action=DescribeRegions&Version=2013-10-15"
-
-    sign = lambda key, msg: hmac.new(key, msg.encode("utf-8"), hashlib.sha256).digest()
-
-    def get_sig_key(key, dateStamp, regionName, serviceName):
-        date = sign(("AWS4" + key).encode("utf-8"), dateStamp)
-        region = sign(date, regionName)
-        service = sign(region, serviceName)
-        signing = sign(service, "aws4_request")
-        return signing
+sage = boto3.Session(
+    aws_access_key_id=os.environ.get("ACCESS_KEY_ID"),
+    aws_secret_access_key=os.environ.get("SECRET_ACCESS_KEY"),
+    region_name="us-east-1",
+).client(service_name="sagemaker-runtime", region_name="us-east-1", use_ssl=False)
+table = boto3.resource("dynamodb").Table(os.environ.get("TABLE_NAME"))
 
 
 def lambda_handler(event, context):
     auth = AWSRequestsAuth(
         aws_access_key=os.environ.get("ACCESS_KEY_ID"),
         aws_secret_access_key=os.environ.get("SECRET_ACCESS_KEY"),
-        aws_host="https://runtime.sagemaker.us-east-1.amazonaws.com/endpoints/acchackendpoint/invocations",
+        aws_host="runtime.sagemaker.us-east-1.amazonaws.com",
         aws_region="us-east-1",
         aws_service="sagemaker",
     )
     print("about to go")
     ok = requests.post(
         url="https://runtime.sagemaker.us-east-1.amazonaws.com/endpoints/acchackendpoint/invocations",
-        data=event["body"],
+        data=base64.b64decode(event["body"]),
         auth=auth,
+        headers={"Content-Type": "image/jpeg"},
+        verify=False,
     )
     print("done")
-    print(ok)
+    print(ok.text)
+    code = list(json.loads(ok.text).keys())[0]
+    table.put_item(Item={
+        "barcode": code,
+        "timestamp": int(time.time())
+        ""
+    })
 
-    # Read AWS access key from env. variables or configuration file. Best practice is NOT
-    # to embed credentials in code.
-    access_key = os.environ.get("ACCESS_KEY_ID")
-    secret_key = os.environ.get("SECRET_ACCESS_KEY")
-
-    return {"statusCode": 200, "body": json.dumps("Helloooooooooo from Lambda!")}
+    return {"statusCode": 200, "body": code}
