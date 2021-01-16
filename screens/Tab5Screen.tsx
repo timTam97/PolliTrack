c0aac218b704c425114562271c6b77b77c8ee66c
Marcus Lim <marcus4lim@gmail.com>
2021-01-16 23:21:20 +1100

    Updated Calandar/Agenda

--- screens/Tab5Screen.tsx
+++ screens/Tab5Screen.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { useState } from 'react';
 import {SafeAreaView, View, Text, Image, StyleSheet, ScrollView, TouchableHighlight, TouchableOpacity} from 'react-native';
-import { Card } from 'react-native-paper';
+import { List } from 'react-native-paper';
 import { Agenda } from 'react-native-calendars';
 
 const timeToString = (time) => {
@@ -11,23 +11,8 @@ const timeToString = (time) => {
 
 export default function Tab5Screen() {
   const [items, setItems] = useState({});
-  const renderItem = (item) => {
-    return (
-      <TouchableOpacity>
-        <Card>
-          <Card.Content>
-            <View style={{ flexDirection: 'row', justifyContent: 'space-between', alignItems: 'center' }}>
-              <Text>{item.name}</Text>
-            </View>
-          </Card.Content>
-        </Card>
-      </TouchableOpacity>
-    );
-  };
-
 return (
     <SafeAreaView style={{ flex: 1 }}>
-      <Text style={styles.title}>sales</Text>
       <View style={styles.agenda}>
       <Agenda
         // The list of items that have to be displayed in agenda. If you want to render item as empty date
@@ -35,11 +20,31 @@ return (
         // considered that the date in question is not yet loaded
         items={{
           '2021-01-16': [{
+            "ProductName": "Sun King Pro 400", // Might get this from a look up table with the pollinategroup with the barcode
             "barcode": "9300633645540",
             "timestamp": "1610792784",
             "image_url": "https://pollitrack-images-asdgybdvkl.s3.amazonaws.com/kIJvN7GdF-W7MQ.jpeg",
             "latitude": -37.862458333333336,
             "longitude": 145.1865527777778,
+            "amountOwed": 120.0,
+          }],
+          '2021-01-17': [{
+            "ProductName": "Sun King Pro 400", // Might get this from a look up table with the pollinategroup with the barcode
+            "barcode": "9300633645540",
+            "timestamp": "1610792784",
+            "image_url": "https://pollitrack-images-asdgybdvkl.s3.amazonaws.com/kIJvN7GdF-W7MQ.jpeg",
+            "latitude": -37.862488333333336,
+            "longitude": 145.1865527777778,
+            "amountOwed": 120.0,
+          }],
+          '2021-01-18': [{
+            "ProductName": "Sun King Pro 400", // Might get this from a look up table with the pollinategroup with the barcode
+            "barcode": "9300633645540",
+            "timestamp": "1610792784",
+            "image_url": "https://pollitrack-images-asdgybdvkl.s3.amazonaws.com/kIJvN7GdF-W7MQ.jpeg",
+            "latitude": -37.862488333333336,
+            "longitude": 145.1865527777778,
+            "amountOwed": 120.0,
           }],
         }}
         // Callback that gets called when items for a certain month should be loaded (month became visible)
@@ -61,7 +66,16 @@ return (
         // Max amount of months allowed to scroll to the future. Default = 50
         futureScrollRange={50}
         // Specify how each item should be rendered in agenda
-        renderItem={({ barcode }) => { return <View style={styles.event}><Text>barcode</Text></View>; }}
+        renderItem={({ ProductName, amountOwed }) => {
+              return (
+                <TouchableOpacity>
+                  <List.Item 
+                    title={"Sold " + ProductName}
+                    description={"Amount Owing: $" + amountOwed}
+                    style={{marginTop: 10, marginBottom: 10, backgroundColor: 'white', borderRadius: 10}} >
+                  </List.Item>
+                </TouchableOpacity>
+              );}}
         // Specify how each date should be rendered. day can be undefined if the item is not first in that day.
         // renderDay={(day, item) => { return (<View />); }}
         // Specify how empty date content with no items should be rendered
@@ -89,18 +103,17 @@ return (
         // Add a custom RefreshControl component, used to provide pull-to-refresh functionality for the ScrollView.
         refreshControl={null}
         // Agenda theme
-        theme={{
-          ...styles.agenda,
-          agendaDayTextColor: 'yellow',
-          agendaDayNumColor: 'green',
-          agendaTodayColor: 'red',
-          agendaKnobColor: 'blue'
-        }}
+        // theme={{
+        //   ...styles.agenda,
+        //   agendaDayTextColor: 'yellow',
+        //   agendaDayNumColor: 'green',
+        //   agendaTodayColor: 'red',
+        //   agendaKnobColor: 'blue'
+        // }}
         // Agenda container style
-        style={{}}
       />
       </View>
-      <Text style={styles.month}>January</Text>
+
     </SafeAreaView>
 
   );
@@ -114,19 +127,11 @@ export const styles = StyleSheet.create({
     marginTop: 25,
     marginLeft: 34,
   },
-  container: {
-    flex: 1,
-  },
-  month: {
-    // fontFamily: 'Lato_700Bold',
-    color: 'rgba(23,34,133,1)',
-    fontSize: 17,
-    top: -530,
-    marginLeft: 34,
-  },
   agenda: {
     flex: 1,
-    top: 0,
+  },
+  container: {
+    flex: 1,
   },
   event: {
     position: 'absolute',
