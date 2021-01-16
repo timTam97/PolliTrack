45846061f6fce998f01ecfc728b03e93ea85d2b9
Chi <cmyip99@gmail.com>
2021-01-16 16:32:11 +1100

    Added capture button

--- screens/TabTwoScreen.tsx
+++ screens/TabTwoScreen.tsx
@@ -1,11 +1,12 @@
-import React, { useState, useEffect } from 'react';
-import { StyleSheet, Text, View, TouchableOpacity, SafeAreaView } from 'react-native';
+import React, { useState, useEffect, useRef } from 'react';
+import { StyleSheet, Text, TouchableOpacity, SafeAreaView, View } from 'react-native';
 import { Camera } from 'expo-camera';
 
 export default function TabTwoScreen() {
   const [hasPermission, setHasPermission] = useState(null);
-  const [cameraRef, setCameraRef] = useState(null);
-  const [type, setType] = useState(Camera.Constants.Type.back);
+  const [setCameraRef] = useState(null);
+  const [type] = useState(Camera.Constants.Type.back);
+  const cameraRef = useRef(null);
 
   useEffect(() => {
     (async () => {
@@ -13,6 +14,14 @@ export default function TabTwoScreen() {
       setHasPermission(status === 'granted');
     })();
   }, []);
+
+  const __takePicture = async () => {
+    const photo: any = await camera.takePictureAsync()
+    console.log(photo)
+    setPreviewVisible(true)
+    //setStartCamera(false)
+    setCapturedImage(photo)
+  }
 
   if (hasPermission === null) {
     return <SafeAreaView />;
@@ -23,16 +32,50 @@ export default function TabTwoScreen() {
   return (
     <SafeAreaView style={styles.container}>
       <Camera style={{ flex: 1 }} type={type} ref={ref => {
-        setCameraRef(ref) ;
+        ref={cameraRef} ;
       }}>
-        <TouchableOpacity style={{alignSelf: 'center'}} onPress={async() => {
-            if(cameraRef){
-              let photo = await cameraRef.takePictureAsync();
-              console.log('photo', photo);
-            }
-        }}>
-        </TouchableOpacity>
-                  
+        <View style={styles.container}>
+          <View
+            style={{
+              position: 'absolute',
+              left: '5%',
+              top: '10%',
+              flexDirection: 'column',
+              justifyContent: 'space-between'
+            }}
+          >
+          </View>
+          <View
+            style={{
+              position: 'absolute',
+              bottom: 0,
+              flexDirection: 'row',
+              flex: 1,
+              width: '100%',
+              padding: 20,
+              justifyContent: 'space-between'
+            }}
+          >
+            <View
+              style={{
+                alignSelf: 'center',
+                flex: 1,
+                alignItems: 'center'
+              }}
+            >
+              <TouchableOpacity
+                onPress={__takePicture}
+                style={{
+                  width: 70,
+                  height: 70,
+                  bottom: 0,
+                  borderRadius: 50,
+                  backgroundColor: '#fff'
+                }}
+              />
+            </View>
+          </View>
+        </View>      
       </Camera>
     </SafeAreaView>
   );
@@ -41,6 +84,10 @@ export default function TabTwoScreen() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+    width: '100%',
+    height: '100%',
+    backgroundColor: 'transparent',
+    flexDirection: 'row'
   },
   camera: {
     flex: 1,
